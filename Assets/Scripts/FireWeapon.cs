20d570f43fc00f3558329cb5a3a163f380ab89b2
poemdexter <poemdexter@gmail.com>
2014-01-05 02:23:20 -0600

    added sprites. audio when shots fired, gun jerks on firing.

---
+++ Assets/Scripts/FireWeapon.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireWeapon : MonoBehaviour
+{
+    public GameObject bullet;
+    public float fireDelay;
+    
+    private bool fired = false;
+    private float currentTime;
+    private float fireTime;
+    
+    void Update()
+    {
+        if (fired && (fireTime += Time.deltaTime) > fireDelay / 2) {
+            fireTime = 0;
+            transform.Translate(new Vector2(.1f, 0));
+            fired = false;
+        }
+        
+        if ((currentTime += Time.deltaTime) > fireDelay) {
+            currentTime = 0;
+            Instantiate(bullet, transform.position + new Vector3(renderer.bounds.size.x, 0, 0), Quaternion.identity);
+            audio.Play();
+            transform.Translate(new Vector2(-.1f, 0));
+            fired = true;
+        }
+    }
+}
---
+++ Assets/Scripts/MoveBullet.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBullet : MonoBehaviour
+{
+    public float speed;
+    
+    private float killTime;
+    
+    void Update()
+    {
+        transform.Translate(new Vector2(speed, 0) * Time.deltaTime);
+        
+        if ((killTime += Time.deltaTime) > 1.0f)
+            Destroy(gameObject);
+    }
+}
