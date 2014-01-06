95c5490cd8ed9e0c316d6e81bcf76161d18b0191
poemdexter <poemdexter@gmail.com>
2014-01-05 22:47:07 -0600

    player knocked back against shooting direction

---
+++ Assets/Scripts/PlayerKnockback.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerKnockback : MonoBehaviour
+{
+    public float power;
+    private Transform gunTransform;
+    
+    void Start()
+    {
+        gunTransform = transform.GetChild(0);
+    }
+	
+    void Update()
+    {
+        Vector3 pushVector = gunTransform.rotation * Vector3.left;
+        rigidbody2D.AddForce(pushVector * power * Time.deltaTime);
+    }
+}
